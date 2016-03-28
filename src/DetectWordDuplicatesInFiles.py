9ce95c802b7537039dbf5f8d3ca4f82e0e7973f5
Robert Marzeta <bink81@gmail.com>
2016-03-28 09:23:07 +0200

    A tool to count duplicated words in text file(s)

---
+++ src/DetectWordDuplicatesInFiles.py
@@ -0,0 +1,52 @@
+'''
+Created 2014
+'''
+import sys, re, collections
+
+def fetchLinesFromFile(file_names):
+	lines = []
+	for file_name in file_names:
+		with open(file_name) as f:
+			lines += f.readlines()
+	return lines
+
+def countWords(lines):
+	wordMap = {}
+	for line in lines:
+		words = re.compile('\w+').findall(line)
+		
+		for word in words:
+			word = word.lower()  # # we also want capitalised repetitions
+			if word in wordMap:
+				wordMap[word] = wordMap[word] + 1
+			else:
+				wordMap[word] = 1
+	return wordMap
+
+def groupDuplicatedWords(wordMap):
+	groupedWordMap = {}
+	for word in wordMap.keys():
+		if wordMap[word] > 1:
+			count = wordMap[word]
+			if not count in groupedWordMap:
+				groupedWordMap[count] = []
+			groupedWordMap[count].append(word)
+	return groupedWordMap	
+
+def printWords(groupedWordMap):
+	sortedWordMap = collections.OrderedDict(sorted(groupedWordMap.items()))
+	print('Count : Words (lower-cased)')
+	for count in sortedWordMap:
+		print(str(count) + "     : " + ', '.join(groupedWordMap[count]))
+
+if __name__ == '__main__':
+	filenames = sys.argv[1:]
+	if not filenames:
+		# For quick testing purposes
+		sys.exit("Missing: a file URL.\nUsage: 'DetectWordDuplicatesInFiles.py <FILE_URL1> <FILE_URL2> <FILE_URL...>'")
+		filenames.append('WordDuplicates.txt')
+	lines = fetchLinesFromFile(filenames)
+	
+	wordMap = countWords(lines)
+	groupedWordMap = groupDuplicatedWords(wordMap)
+	printWords(groupedWordMap)
